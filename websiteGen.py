2f24bbe65ba8ef131ea2199bafe8aa7e0c4b96c5
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-08-17 21:34:49 -0400

    + add more 'warning like' log in the code-gen and also print the text even if an error occurs

--- websiteGen.py
+++ websiteGen.py
@@ -133,7 +133,8 @@ def addRepository(file, username, jsonRepo, isHighlighted, githubToken):
     try:
         file.write("""                <a href=\"""" + jsonRepo["homepage"] + """\">""" + jsonRepo["name"] + """</a>""" + endOfFile)
     except:
-        print("jsonRepo[homepage] might be null for the repository = " + jsonRepo["name"] + ".")
+        file.write("""                <span>""" + jsonRepo["name"] + """</span>""" + endOfFile)
+        print("<!> WARNING: jsonRepo[homepage] might be null for the repository = " + jsonRepo["name"] + ".")
         print("Please check that you added a page to the website section next to the repository description !")
     file.write("""                <img alt="Website" src="https://img.shields.io/website/https/github.com/""" + username + """/""" + jsonRepo["name"] + """.svg\">""" + endOfFile)
     file.write("""              </h2>""" + endOfFile)
