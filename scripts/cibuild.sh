a6ee27a09be651ed3caaf53c3059b5afc2026780
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-11-04 12:29:32 +0100

    + add htmlproofer to the travis build

---
+++ scripts/cibuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Enable error reporting to the console.
+set -e
+
+cd jekyll
+
+# Build the website with the production variable
+JEKYLL_ENV=production bundle exec jekyll build --destination site
+
+# Check that everything is ok with htmlproofer
+bundle exec htmlproofer site
