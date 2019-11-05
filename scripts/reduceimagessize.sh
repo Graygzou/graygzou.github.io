fd91ec2943d6b5fbd098b1a81cc37bca07da1034
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-11-05 21:34:35 +0100

    + add first draft of png compression in the pipeline

--- scripts/reduceimagessize.sh
+++ scripts/reduceimagessize.sh
@@ -3,17 +3,30 @@
 # Enable error reporting to the console.
 set -e
 
+# ====================
+# PNG COMPRESSION
+# ====================
+# For all the png in the project run https://pngquant.org/pngquant.tar.bz2
+curl -o pngquant.tar.bz2 "https://pngquant.org/pngquant.tar.bz2"
+# Create a folder that will hold the tool
+mkdir pngquant
+tar -xvf pngquant.tar.bz2 -C ./pngquant
+# This will create duplicate of images with -fs8 at the end of the file
+find assets/project-images/ -name "*.png" -exec pngquant --force {} \;
+# Remove the previous image not optimized
+find . -not -name "*fs8.png" -exec rm {} \;
+# Rename the file generate to match previous version
+rename "s/fs8//g" assets/project-images/*.png
+
 # If debug is needed, uncomment this line.
 # It will allow to connect remotly to the travis TRAVIS_BUILD_NUMBER
 curl https://www.teleconsole.com/get.sh | sh
 teleconsole
 
-# For all the png in the project run https://pngquant.org/pngquant.tar.bz2
-# TODO find -depth -name "*.png"
-
+# ====================
+# JPG COMPRESSION
+# ====================
 # For all the jpg in the project run Guetzli.
 # See https://github.com/google/guetzli for more info
-brew install guetzli
-
 # Run the exe for each jpg found
 #find -depth -name "*.jpg" -exec guetzli --verbose {} {}
