5dbb6f520aabef4aaba5873c4b2c13ec7ec8595d
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-11-06 21:33:19 +0100

    = fix the path for png compression

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -13,13 +13,13 @@ mkdir pngquant
 tar -xvf pngquant.tar.bz2 -C ./pngquant
 # This will create duplicate of images with -fs8 at the end of the file
 echo "Run pngquant command"
-find ./jekyll/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
+find ../jekyll/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
 # Remove the previous image not optimized
 echo "Remove the previous image not optimized"
-find ./jekyll/assets/project-images/ -not -name "*fs8.png" -exec rm {} \;
+find ../jekyll/assets/project-images/ -not -name "*fs8.png" -exec rm {} \;
 # Rename the file generate to match previous version
 echo "Rename compressed images to match declarations"
-rename "s/fs8//g" ./jekyll/assets/project-images/*.png
+rename "s/fs8//g" ../jekyll/assets/project-images/*.png
 
 # If debug is needed, uncomment this line.
 # It will allow to connect remotly to the travis TRAVIS_BUILD_NUMBER
