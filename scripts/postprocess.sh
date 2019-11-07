7255e05d947a91fe9b8c9c85359d6c2a0c46b16d
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-11-07 15:34:51 +0100

    + correct some minor issues on CV and fix path to compress png in the site folder (live).

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -8,13 +8,13 @@ set -e
 # ====================
 # This will create duplicate of images with -fs8 at the end of the file
 echo "Run pngquant command"
-find ./jekyll/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
+find site/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
 # Remove the previous image not optimized
 echo "Remove the previous image not optimized"
-find ./jekyll/assets/project-images/ -not -name "*fs8.png" -exec rm {} \;
+find site/assets/project-images/ -not -name "*fs8.png" -exec rm {} \;
 # Rename the file generate to match previous version
 echo "Rename compressed images to match declarations"
-rename "s/fs8//g" ./jekyll/assets/project-images/*.png
+rename "s/fs8//g" site/assets/project-images/*.png
 
 # If debug is needed, uncomment this line.
 # It will allow to connect remotly to the travis TRAVIS_BUILD_NUMBER
