b2ae5b84c4e25ce0f9ce372e94114a629a497b34
Gregoire Boiron <gregoire.boiron@gmail.com>
2019-11-12 13:06:23 +0100

    + add jpg encoder to reduce jpg image size automatically

--- scripts/postprocess.sh
+++ scripts/postprocess.sh
@@ -6,32 +6,32 @@ set -e
 #curl https://www.teleconsole.com/get.sh | sh
 #teleconsole
 
-# ====================
-# PNG COMPRESSION
-# ====================
+# ========================================
+# PNG COMPRESSION WITH PNGQUANT
+# ========================================
 # This will create duplicate of images with -fs8 at the end of the file
 echo "Run pngquant command"
 find jekyll/site/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
+
 # Remove the previous image not optimized
 echo "Remove the previous image not optimized"
 find jekyll/site/assets/project-images/ -name "*.png" -not -name "*fs8.png" -exec rm {} \;
+
 # Rename the file generate to match previous version
 echo "Rename compressed images to match declarations"
 find jekyll/site/assets/project-images/ -name "*.png" -exec rename "s/-fs8//g" {} \;
 
 # If debug is needed, uncomment this line.
 # It will allow to connect remotly to the travis TRAVIS_BUILD_NUMBER
-curl https://www.teleconsole.com/get.sh | sh
-teleconsole
+# curl https://www.teleconsole.com/get.sh | sh
+# teleconsole
 
-# ====================
-# JPG COMPRESSION
-# ====================
+# ========================================
+# JPG COMPRESSION WITH GUETZLI
+# ========================================
 # For all the jpg in the project run Guetzli.
 # See https://github.com/google/guetzli for more info
-# Run the exe for each jpg found
-#find -depth -name "*.jpg" -exec guetzli --verbose {} {}
-
+find jekyll/site/assets/ -name "*.jpg" -exec guetzli --verbose {} {} \;
 
 # ====================
 # HTML PROOFER
