d8ea164baee8871fe037a7cef3c9c3ac547e2197
Graygzou-bot (from Travis CI) <boiron.greg@gmail.com>
2019-11-27 14:27:50 +0000

    Deploy Graygzou/graygzou.github.io to github.com/Graygzou/graygzou.github.io.git:master

---
+++ scripts/service-worker.js
@@ -0,0 +1,61 @@
+const staticCacheName = "graygzou-portfolio";
+
+const filesToCache = [
+    "/",
+    '/404.html',
+    '/about/',
+    '/blog/',
+    '/',
+    '/projects/',
+    '/resume/',
+    
+    '/blog/challenges/2019/10/10/advent-of-code-2018.html',
+    '/blog/c++/2019/09/28/tips-and-tricks-ccp.html',
+    '/blog/jekyll/update/2019/07/28/getting-start-with-jekyll.html'
+];
+
+/*
+<!-- "/assets/images/bhavri-github-callbacks.png",
+    "/assets/images/bhavri-github-issues.png",
+    "/assets/images/jakethecake-svg-line-anime.png",
+    "/assets/images/svg-animated-mast-text-shapes-tweet.png",
+    "css/main.css",
+    "/about/",
+    "/index.html" -->*/
+
+console.log("installing service worker");
+
+self.addEventListener("install", function(e){
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(staticCacheName).then(function(cache){
+            filesToCache.map(function(url){
+                return cache.add(url).catch(function (reason) {
+                    return console.log(url + 'failed: ' + String(reason));
+                });
+            })
+        })
+    )
+});
+
+self.addEventListener("activate", function(e){
+    e.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName.startsWith("graygzou-portfolio") && cacheName != staticCacheName;
+                }).map(function(cacheName){
+                    return caches.delete(cacheName);
+                })
+            )
+        })
+    )
+});
+
+self.addEventListener("fetch", function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    )
+});
