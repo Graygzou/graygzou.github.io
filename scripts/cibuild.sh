165970f1e4605a8ec2a7b61ec4465bed8b364055
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-01-11 18:59:15 +0100

    + push a fix for the lighthouse checks

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -9,9 +9,19 @@
 
 cd jekyll
 
+# The build that will be deploy
 echo "travis_fold:start:jekyll_production_build"
 echo "build jekyll site with the production variable"
 # Build the website with the production variable
-JEKYLL_ENV=production bundle exec jekyll build --destination site
+bundle exec jekyll build JEKYLL_ENV=production --destination site
+echo "travis_fold:end:jekyll_production_build"
+echo "jekyll build done."
+
+# Useful to run lighthouse tests on it
+# /!\ Don't forget to kill the process created !
+echo "travis_fold:start:jekyll_local_build"
+echo "build jekyll site with the production variable"
+# Build the website with the production variable
+bundle exec jekyll serve --detach
 echo "travis_fold:end:jekyll_production_build"
 echo "jekyll build done."
