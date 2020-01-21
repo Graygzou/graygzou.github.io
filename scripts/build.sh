205b96d0dd99a4b6fdfdc7032834856fb0855047
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-01-21 19:46:00 +0100

    + add stages section to specify branch execution directly

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,4 +20,13 @@ bundle exec jekyll build JEKYLL_ENV=production --destination site
 echo "travis_fold:end:jekyll_production_build"
 echo "jekyll build done."
 
+# Run html-proofer to see if there is any errors
+echo "travis_fold:start:run_html_proofer"
+echo "Start html proofer check"
+# Check that everything is ok with htmlproofer
+# Must first enter jekyll folder to access the Gemfile.
+cd jekyll
+bundle exec htmlproofer site --url-ignore "https://www.linkedin.com/in/gregoire-boiron/,https://www.latecoere.aero/en/"
+echo "travis_fold:end:run_html_proofer"
+
 echo "build.sh script done."
--- scripts/html-proofer.sh
+++ scripts/html-proofer.sh
@@ -7,17 +7,8 @@
 # html-proofer
 #############################################################################
 
-# The -e flag causes the script to exit as soon as one command returns a non-zero exit code
-# The -v flag makes the shell print all lines in the script before executing them, which helps identify which steps failed.
-set -ev
 
-# Run the program
-echo "travis_fold:start:run_html_proofer"
-echo "Start html proofer check"
-# Check that everything is ok with htmlproofer
-# Must first enter jekyll folder to access the Gemfile.
-cd jekyll
-bundle exec htmlproofer site --url-ignore "https://www.linkedin.com/in/gregoire-boiron/,https://www.latecoere.aero/en/"
-echo "travis_fold:end:run_html_proofer"
+
+
 
 echo "html-proofer.sh script done."
