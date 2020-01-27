d9a29a79b7fa8c4d939749827f5185b13f6e9e26
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-01-27 21:18:00 +0100

    + edit the deploy job to upload site on the release branch

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#############################################################################
+# Grégoire Boiron <gregoire.boiron@gmail.com>
+# Copyright (c) 2018-2020 Grégoire Boiron  All Rights Reserved.
+#
+# deploy
+#############################################################################
+
+cd jekyll
+
+#---------------------------------------------------------------
+# The build that will be deploy to github-page
+#
+# Build the website with the production variable
+#---------------------------------------------------------------
+echo "travis_fold:start:jekyll_production_build"
+echo "build jekyll site with the production variable"
+bundle exec jekyll build JEKYLL_ENV=production --destination site
+echo "travis_fold:end:jekyll_production_build"
+echo "jekyll build done."
+
+# Upload back to github the artifacts created
+echo "travis_fold:start:push_webp"
+echo "push new webp images to the branch"
+./scripts/upload-new-file.sh "site/*" "release"
+echo "travis_fold:end:push_webp"
+
+echo "✅ deploy.sh script done."
--- scripts/upload-new-file.sh
+++ scripts/upload-new-file.sh
@@ -16,15 +16,27 @@ fi
 # Retrieve the pattern provided by the user
 pattern=$1
 
-echo "travis_fold:start:upload_to_github"
-echo "Upload files to the ${TRAVIS_BRANCH} branch"
+# change branch if necessary
+echo "travis_fold:start:checkout_branch"
+echo "Change branch if necessary"
+if [ "$?" -gt 1 ]; then 
+  git checkout $2
+fi
+echo "travis_fold:end:checkout_branch"
+
 # upload to github if necessary
+echo "travis_fold:start:config_user"
+echo "Change branch if necessary"
 git config user.email ${GITHUB_BOT_MAIL}
 git config user.name ${GITHUB_BOT_NAME}
-git add $pattern
+echo "travis_fold:end:config_user"
 
-# Check to avoid extra commit if not necessary   
+# Check to avoid extra commit if not necessary 
+echo "travis_fold:start:upload_to_github"
+echo "Upload files to the ${TRAVIS_BRANCH} branch"
+git add $pattern
 NB_FILE_CHANGED="$(git status --porcelain | grep ^[AM] | wc -l)"
+
 if [ "${NB_FILE_CHANGED}" -gt 0 ]; then
     git commit -a -m "[skip travis][ignore] Upload files generate by the job #$TRAVIS_JOB_NUMBER $TRAVIS_JOB_NAME : $TRAVIS_JOB_WEB_URL"
     git push
