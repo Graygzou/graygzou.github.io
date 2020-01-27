08294644596d212df28e3f0389f47691db9b891d
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-01-27 19:03:10 +0100

    - remove extra job and fix lighthouse one

--- scripts/lighthouse-ci.sh
+++ scripts/lighthouse-ci.sh
@@ -2,14 +2,10 @@
 
 #############################################################################
 # Grégoire Boiron <gregoire.boiron@gmail.com>
-# Copyright (c) 2018-2019 Grégoire Boiron  All Rights Reserved.
+# Copyright (c) 2018-2020 Grégoire Boiron  All Rights Reserved.
 #
 # Lighthouse-ci
 #############################################################################
-
-# The -e flag causes the script to exit as soon as one command returns a non-zero exit code
-# The -v flag makes the shell print all lines in the script before executing them, which helps identify which steps failed.
-set -ev
 
 # Download the package
 echo "travis_fold:start:install_lighthouse_ci"
@@ -17,10 +13,17 @@ echo "install lighthouse-ci"
 npm install -g @lhci/cli@0.3.x
 echo "travis_fold:end:install_lighthouse_ci"
 
+# Deploy the website locally so lighthouse can run tests on it
+# --detach option let us run commands after serving the site
+echo "travis_fold:start:run_local_build"
+echo "build jekyll site in local to test it"
+( cd jekyll && bundle exec jekyll serve --detach \;)
+echo "travis_fold:end:run_local_build"
+
 # Create the config for lighthouse-ci
 echo "travis_fold:start:init_lighthouse_config"
 echo "create lighthouse-ci config"
-(cd scripts && python getAllWebsitePages.py 0 "../jekyll/_site/sitemap.xml" \;)
+( cd scripts && python getAllWebsitePages.py 0 "../jekyll/_site/sitemap.xml" \;)
 echo "travis_fold:end:init_lighthouse_config"
 
 # Make sure the config is safe
@@ -28,13 +31,6 @@ echo "travis_fold:start:healthcheck_lighthouse"
 echo "healthcheck lighthouse-ci"
 lhci healthcheck --fatal --checks=githubToken
 echo "travis_fold:end:healthcheck_lighthouse"
-
-# Deploy the website locally so lighthouse can run tests on it
-# --detach option let us run commands after serving the site
-echo "travis_fold:start:run_local_build"
-echo "build jekyll site with the production variable"
-bundle exec jekyll serve --detach
-echo "travis_fold:end:run_local_build"
 
 # Run the tests
 echo "travis_fold:start:start_lighthouse"
