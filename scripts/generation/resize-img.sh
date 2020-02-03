8dae3e38575a6a7365957dbf41c7ea446ab95c15
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-02-03 20:41:47 +0100

    + add imageMagick job to resize images down for optimization.

---
+++ scripts/generation/resize-img.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#############################################################################
+# Grégoire Boiron <gregoire.boiron@gmail.com>
+# Copyright (c) 2018-2020 Grégoire Boiron  All Rights Reserved.
+#
+# Run ImageMagick algorithm to create different versions of all images
+#############################################################################
+
+# Try to find if any file matching the provided extension
+jpgResult=$( ./scripts/helpers/file-changed-in-last-commit.sh "*\.jpg$" )
+pngResult=$( ./scripts/helpers/file-changed-in-last-commit.sh "*\.png$" )
+
+echo "$jpgResult"
+echo "$pngResult"
+
+if [[ "$jpgResult" -ne 1 ]] || [[ "$pngResult" -ne 1 ]] ; then
+  # Download the package
+  #echo "travis_fold:start:install_webp"
+  #echo "install webp for webp encoding"
+  #sudo apt-get install webp
+  #echo "travis_fold:end:install_webp"
+
+  # Run ImageMagick algorithm
+  echo "travis_fold:start:imageMagick"
+  echo "Start running imageMagick"
+  find jekyll/assets/ \( -name "*.jpg" -o -name "*.png" \) -exec magick convert {} -resize 300x300 300-{} \;
+  echo "travis_fold:end:imageMagick"
+  
+  # Upload back to github the artifacts created
+  echo "travis_fold:start:push_resize"
+  echo "push new resized images to the branch"
+  ./scripts/helpers/upload-new-file.sh "^300-*"
+  echo "travis_fold:end:push_resize"
+else
+  echo "⏭️ No jpg or png in the last commit. Job skipped."
+fi
+echo "✅ resize-img.sh script done."
