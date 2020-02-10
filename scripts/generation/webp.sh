0ccd7c2f66d630212e148167cfd9cb7d03e97780
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-02-10 23:25:06 +0100

    + make the webp creation script upload images and correct the path too

--- scripts/generation/webp.sh
+++ scripts/generation/webp.sh
@@ -21,13 +21,21 @@ if [[ "$jpgResult" -ne 1 ]] || [[ "$pngResult" -ne 1 ]] ; then
   sudo apt-get install webp
   echo "travis_fold:end:install_webp"
 
+  echo "travis_fold:start:clone_project"
+  echo "Clone project"
+  git clone https://${GITHUB_BOT_NAME}:${BOT_DEPLOY_TOKEN}@github.com/Graygzou/graygzou.github.io.git ../graygzou.github.io.develop
+  cd ../graygzou.github.io.develop
+  echo "travis_fold:end:clone_project"
+
+  chmod +x scripts/*/*.sh
+
   # Encode images using cwebp
   echo "travis_fold:start:run_cwebp"
   echo "Start running cwebp for webp image creation"
   # Use cwebp to encode all asset images. see https://developers.google.com/speed/webp/docs/precompiled
   find jekyll/assets/ \( -name "*.jpg" -o -name "*.png" \) -exec cwebp {} -o {}.webp \;
-  mkdir webp
-  find jekyll/assets/ \( -name "*.webp" \) -exec mv {} ./webp/ \;
+  mkdir jekyll/assets/webp
+  find jekyll/assets/ \( -name "*.webp" \) -exec mv {} ./jekyll/assets/webp/ \;
   find jekyll/assets/webp/ -name "*.webp" -exec rename "s/\.png|\.jpg//g" {} \; -exec rm {} \;
   echo "travis_fold:end:run_cwebp"
   
