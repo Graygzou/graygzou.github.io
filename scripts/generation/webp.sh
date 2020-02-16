8b2e9d1550d535c88653fc537a09bf7d74045f9b
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-02-16 20:26:24 +0100

    = fix different syntax errors raised during last build

--- scripts/generation/webp.sh
+++ scripts/generation/webp.sh
@@ -46,14 +46,14 @@ if [[ "$jpgResult" -ne 1 ]] || [[ "$pngResult" -ne 1 ]] ; then
   echo "Start running cwebp for webp image creation"
   # Use cwebp to encode all asset images except for the folder ./originals. 
   # see https://developers.google.com/speed/webp/docs/precompiled
-  find jekyll/assets/ -type -d -path ./originals -prune -o -name "*.jpg" -o -name "*.png" -print -exec cwebp {} -o {}.webp \;
+  find jekyll/assets/ -type d -path ./originals -prune -o -name "*.jpg" -o -name "*.png" -print -exec cwebp {} -o {}.webp \;
   echo "travis_fold:end:run_cwebp"
 
   # Move images and format them
   echo "travis_fold:start:rename_webp_images"
   echo "Renamed new images and move them in a separated folder"
   find jekyll/assets/ -name "*.webp" -exec mv {} jekyll/assets/webp/ \;
-  \( cd jekyll/assets/webp/ && rename -v 's/.png.webp|.jpg.webp/.webp/gi' *.webp \; \)
+  cd jekyll/assets/webp/ && rename -v 's/.png.webp|.jpg.webp/.webp/gi' *.webp \;
   find jekyll/assets/webp/ \( -name "*.png.webp" -o -name "*.jpg.webp" \) -exec rm -v {} \;
   echo "travis_fold:end:rename_webp_images"
   
