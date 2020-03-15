ee867dcbfc0ce4f8adad1715140bb4a0de498239
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-03-15 14:44:34 +0100

    + add commands to switch remote branch from https to ssh to allow pushing to it

--- scripts/helpers/branching.sh
+++ scripts/helpers/branching.sh
@@ -18,7 +18,18 @@ git config user.email ${GITHUB_BOT_MAIL}
 git config user.name ${GITHUB_BOT_NAME}
 echo "travis_fold:end:config_user"
 
+echo "travis_fold:start:change_remote"
+echo "Change remote from https to ssh"
+git remote -v
+git remote remove origin
+git remote add origin git@github.com:user/repo.git
+echo "travis_fold:end:change_remote"
+
+git status
+
 echo "travis_fold:start:push_branch_origin"
 echo "Push created branch so other job can use it when pushing"
 git push -u origin travis-build-${TRAVIS_BUILD_NUMBER}
 echo "travis_fold:end:push_branch_origin"
+
+git status
