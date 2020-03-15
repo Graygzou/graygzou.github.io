c19e3abb0f2ff2615d9ff1e407ebb75dd868e8f9
Gregoire Boiron <gregoire.boiron@gmail.com>
2020-03-15 15:32:56 +0100

    + add git clone to be able to push with my github bot

--- scripts/helpers/branching.sh
+++ scripts/helpers/branching.sh
@@ -7,22 +7,29 @@
 # create branch to gather all commits made by travis jobs
 #############################################################################
 
-echo "travis_fold:start:create_new_branch"
-echo "Create new branch dedicated for the build ${TRAVIS_BUILD_NUMBER}"
-git branch travis-build-${TRAVIS_BUILD_NUMBER}
-echo "travis_fold:end:create_new_branch"
-
 echo "travis_fold:start:config_user"
 echo "Config github bot user"
 git config user.email ${GITHUB_BOT_MAIL}
 git config user.name ${GITHUB_BOT_NAME}
 echo "travis_fold:end:config_user"
 
+echo "travis_fold:start:clone_project"
+echo "Clone project"
+git clone https://${GITHUB_BOT_NAME}:${BOT_DEPLOY_TOKEN}@github.com/Graygzou/graygzou.github.io.git ../graygzou.github.io.cloned
+cd ../graygzou.github.io.cloned
+echo "travis_fold:end:clone_project"
+
+echo "travis_fold:start:create_new_branch"
+echo "Create new branch dedicated for the build ${TRAVIS_BUILD_NUMBER}"
+git branch travis-build-${TRAVIS_BUILD_NUMBER}
+echo "travis_fold:end:create_new_branch"
+
 echo "travis_fold:start:change_remote"
 echo "Change remote from https to ssh"
 git remote -v
 git remote remove origin
 git remote add origin git@github.com:user/repo.git
+git remote -v
 echo "travis_fold:end:change_remote"
 
 git status
