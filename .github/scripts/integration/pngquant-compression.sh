0e1ac4aa928130a35531b6cf3fd7211b4b8f0c28
Grégoire Boiron <Graygzou@users.noreply.github.com>
2021-04-26 13:24:44 +0200

    Adapt pngquant script with correct path and group declarations

--- .github/scripts/integration/pngquant-compression.sh
+++ .github/scripts/integration/pngquant-compression.sh
@@ -2,44 +2,30 @@
 
 #############################################################################
 # Grégoire Boiron <gregoire.boiron@gmail.com>
-# Copyright (c) 2018-2019 Grégoire Boiron  All Rights Reserved.
+# Copyright (c) 2018-2021 Grégoire Boiron  All Rights Reserved.
 #
 # Script that run pngquant.
 #############################################################################
 
-# Try to find if any file matching the provided extension
-pngResult=$( ./scripts/helpers/file-changed-in-last-commit.sh "*.png" )
+# Install the package
+echo "::group::install_pngquant"
+echo "install pngquant for png compression"
+sudo apt-get install pngquant
+echo "::endgroup::"
 
-echo "$pngResult"
+# Run the package
+echo "::group::run_pngquant"
+echo "Run pngquant command"
+# This will create duplicate of images with -fs8 at the end of the file
+find ./jekyll/assets/project-images/ -name "*.png" -not -name "*-opti.png" -exec pngquant --ext -opti.png {} \;
 
-if [[ "$pngResult" -ne 0 ]] ; then
-    # Install the package
-    echo "travis_fold:start:install_pngquant"
-    echo "install pngquant for png compression"
-    sudo apt-get install pngquant
-    echo "travis_fold:end:install_pngquant"
+# Remove the previous image not optimized
+echo "Remove the previous image not optimized"
+find ./jekyll/assets/project-images/ -name "*.png" -not -name "*-opti.png" -exec rm {} \;
+echo "::endgroup::"
 
-    # Run the package
-    echo "travis_fold:start:run_pngquant"
-    echo "Run pngquant command"
-    # This will create duplicate of images with -fs8 at the end of the file
-    find ./jekyll/site/assets/project-images/ -name "*.png" -exec pngquant --force {} \;
+# Rename the file generate to match previous version
+# echo "Rename compressed images to match declarations"
+# find ./jekyll/assets/project-images/ -name "*.png" -exec rename "s/-fs8//g" {} \;
 
-    # Remove the previous image not optimized
-    echo "Remove the previous image not optimized"
-    find ./jekyll/site/assets/project-images/ -name "*.png" -not -name "*fs8.png" -exec rm {} \;
-
-    # Rename the file generate to match previous version
-    echo "Rename compressed images to match declarations"
-    find ./jekyll/site/assets/project-images/ -name "*.png" -exec rename "s/-fs8//g" {} \;
-    echo "travis_fold:end:run_pngquant"
-
-    # Upload artifacts to the repo
-    echo "travis_fold:start:push_results"
-    echo "push compressed png images to the branch"
-    ./scripts/helpers/upload-new-file.sh "*.png"
-    echo "travis_fold:end:push_results"
-else
-  echo "⏭️ No png in the last commit. Job skipped."
-fi
 echo "✅ pngquant-compression.sh script done."
